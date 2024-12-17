d5999b7c8f932328c42bb5a7fb9950e45eb898e8
bbivy <yvettebiog@gmail.com>
2024-12-17 11:07:42 +0100

    push

---
+++ src/moltiplicazionex2/Moltiplicazionex2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package moltiplicazionex2;
+
+/**
+ *
+ * @author ospite
+ */
+public class Moltiplicazionex2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
